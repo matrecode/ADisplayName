e8a5df376405c015298ea028e75975cf4dccf976
Akshay Matre <99554130+matrecode@users.noreply.github.com>
2024-04-25 04:30:21 +0000

    developed displayname

---
+++ src/DisplayName.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const DisplayName = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleChangeFirstName = (e) => {
+    setFirstName(e.target.value);
+    // console.log(firstName);
+  };
+
+  const handleChangeLastName = (e) => {
+    setLastName(e.target.value);
+    // console.log(lastName);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsSubmitted(true);
+  };
+
+  return (
+    <>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="firstName"
+          id="firstName"
+          value={firstName}
+          onChange={handleChangeFirstName}
+          required
+        />
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          value={lastName}
+          onChange={handleChangeLastName}
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {isSubmitted && (
+        <p>
+          Full Name : {firstName} {lastName}
+        </p>
+      )}
+    </>
+  );
+};
+
+export default DisplayName;
