95e6d850106e71b1ce1fd254955eafa90d19d8ec
matrecode <akshaymatrecode@gmail.com>
2024-04-25 17:58:24 +0530

    implemented logic for calculations

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Calculator.module.css";
 
 const Calculator = () => {
@@ -20,14 +20,39 @@ const Calculator = () => {
     "=",
     "/",
   ];
+
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleClick = (value) => {
+    if (value === "=") {
+      try {
+        setResult(eval(input));
+      } catch (error) {
+        setResult("Error");
+      }
+    } else if (value === "C") {
+      setInput("");
+      setResult("");
+    } else {
+      setInput(input + value);
+    }
+  };
+
   return (
     <>
       <h1>React Calculator</h1>
-      <input type="text" name="text" />
+      <input type="text" value={input} name="text" readOnly />
+      <p>{result}</p>
       <div className={styles.btnContainer}>
         {buttonsArray.map((btn) => {
           return (
-            <button type="button" className={styles.btns} key={btn}>
+            <button
+              type="button"
+              onClick={() => handleClick(btn)}
+              className={styles.btns}
+              key={btn}
+            >
               {btn}
             </button>
           );
