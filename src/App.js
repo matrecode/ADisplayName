669997460519b215ebd6a5a080dc0f34aa1440a2
matrecode <akshaymatrecode@gmail.com>
2024-04-27 13:09:07 +0530

    implemented counter in class comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import Calculator from "./components/Calculator/Calculator";
 // import Stopwatch from "./components/Stopwatch/Stopwatch";
 // import Dropdown from "./components/Dropdowns/Dropdown";
-import Login from "./components/Login/Login";
+// import Login from "./components/Login/Login";
 import "./styles.css";
 // import DisplayName from "./DisplayName.jsx";
 // import Counter from "./components/Counter/Counter.jsx";
+import CounterClass from "./components/ClassCounter/CounterClass";
 export default function App() {
   return (
     <div>
@@ -13,7 +14,8 @@ export default function App() {
 
       {/* <Stopwatch /> */}
       {/* <Dropdown /> */}
-      <Login />
+      {/* <Login /> */}
+      <CounterClass />
     </div>
   );
 }
---
+++ src/components/ClassCounter/CounterClass.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+class CounterClass extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
+  increment() {
+    this.setState((prev) => ({
+      count: prev.count + 1,
+    }));
+  }
+
+  decrement() {
+    this.setState((prev) => ({
+      count: prev.count - 1,
+    }));
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Counter App</h1>
+        <p>Counter: {this.state.count}</p>
+
+        <div>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.decrement}>Decrement</button>
+        </div>
+      </>
+    );
+  }
+}
+
+export default CounterClass;
